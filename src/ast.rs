b48dfe5f63b5f57a8d4fd1d780f90c953b256236
Elliot Lockerman <elliot@lockerman.info>
2024-09-02 10:26:58 -0400

    Optional expression cli argument for non-repl mode; moved existing tests to ast.rs.

--- src/ast.rs
+++ src/ast.rs
@@ -70,5 +70,100 @@ impl Expr {
             Or(l, r) => l.eval() | r.eval(),
         }
     }
-
 }
+
+
+#[cfg(test)]
+mod tests {
+    use crate::ast;
+    use crate::grammar::ExprParser;
+
+    use std::assert_matches::assert_matches;
+
+    use lalrpop_util::ParseError;
+
+    #[test]
+    fn simple() {
+        let parser = ExprParser::new();
+
+        assert_eq!(parser.parse("1").unwrap().eval(), 1);
+        assert_eq!(parser.parse("(1)").unwrap().eval(), 1);
+        assert_eq!(parser.parse("(1 + 1)").unwrap().eval(), 2);
+        assert_eq!(parser.parse("(1) + 1").unwrap().eval(), 2);
+        assert_eq!(parser.parse("1 + (1)").unwrap().eval(), 2);
+        assert_eq!(parser.parse("10 + 2 * 5").unwrap().eval(), 20);
+        assert_eq!(parser.parse("(10 + 2) * 5").unwrap().eval(), 60);
+
+        assert_eq!(parser.parse("10/3").unwrap().eval(), 3);
+        assert_eq!(parser.parse("10 % 3").unwrap().eval(), 1);
+
+        assert_eq!(parser.parse("2 & 1").unwrap().eval(), 0);
+        assert_eq!(parser.parse("2 | 1").unwrap().eval(), 3);
+        assert_eq!(parser.parse("3 ^ 1").unwrap().eval(), 2);
+        assert_eq!(parser.parse("1 | 6 ^ 7 & 12").unwrap().eval(), 3);
+        assert_eq!(parser.parse("(1 | 6) ^ 7 & 12").unwrap().eval(), 3);
+        assert_eq!(parser.parse("1 | (6 ^ 7) & 12").unwrap().eval(), 1);
+
+        assert_eq!(parser.parse("3 << 2").unwrap().eval(), 12);
+        assert_eq!(parser.parse("12 >> 2").unwrap().eval(), 3);
+    }
+
+    #[test]
+    fn malformed() {
+        let parser = ExprParser::new();
+
+        assert_matches!(
+            parser.parse("1000000000000").unwrap_err(),
+            ParseError::User{error: ast::Error::LitParse(_)},
+        );
+        assert_matches!(
+            parser.parse("0xg").unwrap_err(),
+            ParseError::InvalidToken{..},
+        );
+        assert_matches!(
+            parser.parse("0x1000000000000").unwrap_err(),
+            ParseError::User{error: ast::Error::LitParse(_)},
+        );
+        assert_matches!(
+            parser.parse("0o9").unwrap_err(),
+            ParseError::InvalidToken{..},
+        );
+        assert_matches!(
+            parser.parse("0o1000000000000").unwrap_err(),
+            ParseError::User{error: ast::Error::LitParse(_)},
+        );
+        parser.parse("10 + 1)").unwrap_err();
+        parser.parse("10 ++ 1)").unwrap_err();
+        parser.parse("10 += 1)").unwrap_err();
+        parser.parse("(10 + 1").unwrap_err();
+        parser.parse("10)( + 1").unwrap_err();
+        parser.parse("10() + 1").unwrap_err();
+    }
+
+    #[test]
+    fn unary() {
+        let parser = ExprParser::new();
+
+        assert_eq!(parser.parse("-1").unwrap().eval(), -1i32 as u32);
+        assert_eq!(parser.parse("-0").unwrap().eval(), -0i32 as u32);
+        assert_eq!(parser.parse("-5 - 6").unwrap().eval(), (-5i32 as u32).wrapping_sub(6));
+        assert_eq!(parser.parse("-5 + 6").unwrap().eval(), (-5i32 as u32).wrapping_add(6));
+        assert_eq!(parser.parse("-5 + -6").unwrap().eval(), (-5i32 as u32).wrapping_add(-6i32 as u32));
+
+        assert_eq!(parser.parse("!0").unwrap().eval(), !0);
+        assert_eq!(parser.parse("!1").unwrap().eval(), !1);
+        assert_eq!(parser.parse("!32").unwrap().eval(), !32);
+        assert_eq!(parser.parse("!(-32)").unwrap().eval(), !(-32i32 as u32));
+    }
+
+    #[test]
+    fn radix_literal() {
+        let parser = ExprParser::new();
+
+        assert_eq!(parser.parse("0xf").unwrap().eval(), 15);
+        assert_eq!(parser.parse("0o20").unwrap().eval(), 16);
+        assert_eq!(parser.parse("0xf ^ 0o20").unwrap().eval(), 31);
+    }
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,49 @@
 
 mod ast;
 
+use std::process::ExitCode;
+use std::thread_local;
+
 use lalrpop_util::lalrpop_mod;
 lalrpop_mod!(grammar, "/grammar.rs");
 
 use rustyline::{DefaultEditor, error::ReadlineError};
+use clap::Parser;
 
 
+#[derive(Parser, Debug)]
+struct Args {
+    expr: Option<String>,
+}
 
-fn main() {
+fn eval(expr: &str) -> Result<u32, String> {
+    thread_local! {
+        static PARSER: grammar::ExprParser = Default::default();
+    }
+
+    match PARSER.with(|p| p.parse(expr)) {
+        Ok(expr) => Ok(expr.eval()),
+        Err(e) => Err(e.to_string()),
+    }
+}
+
+fn main() -> ExitCode {
+
+    let args = Args::parse();
+
+    if let Some(expr) = &args.expr {
+        return match eval(expr) {
+            Ok(val) => {
+                println!("{val}");
+                ExitCode::SUCCESS
+            },
+            Err(msg) => {
+                eprintln!("{msg}");
+                ExitCode::FAILURE
+            },
+        }
+    }
+
     let mut rl = DefaultEditor::new().unwrap();
 
     loop {
@@ -21,113 +56,17 @@ fn main() {
                 if line.chars().all(|ch| ch.is_whitespace()) {
                     continue; 
                 }
-                let expr = match grammar::ExprParser::new().parse(&line) {
-                    Ok(p) => p,
-                    Err(e) => {
-                        eprintln!("{}", e);
-                        continue;
-                    },
-                };
-       
-                println!("{}", expr.eval());
+                match eval(&line) {
+                    Ok(val) => println!("{val}"),
+                    Err(msg) => eprintln!("{msg}"),
+                }
+
             },
             Err(ReadlineError::Interrupted)| Err(ReadlineError::Eof) => break,
             Err(err) => println!("Error: {:?}", err),
         }
     }
+
+    ExitCode::SUCCESS
 }
-
-#[cfg(test)]
-mod tests {
-    use crate::ast;
-    use crate::grammar::ExprParser;
-
-    use std::assert_matches::assert_matches;
-
-    use lalrpop_util::ParseError;
-
-    #[test]
-    fn simple() {
-        let parser = ExprParser::new();
-
-        assert_eq!(parser.parse("1").unwrap().eval(), 1);
-        assert_eq!(parser.parse("(1)").unwrap().eval(), 1);
-        assert_eq!(parser.parse("(1 + 1)").unwrap().eval(), 2);
-        assert_eq!(parser.parse("(1) + 1").unwrap().eval(), 2);
-        assert_eq!(parser.parse("1 + (1)").unwrap().eval(), 2);
-        assert_eq!(parser.parse("10 + 2 * 5").unwrap().eval(), 20);
-        assert_eq!(parser.parse("(10 + 2) * 5").unwrap().eval(), 60);
-
-        assert_eq!(parser.parse("10/3").unwrap().eval(), 3);
-        assert_eq!(parser.parse("10 % 3").unwrap().eval(), 1);
-
-        assert_eq!(parser.parse("2 & 1").unwrap().eval(), 0);
-        assert_eq!(parser.parse("2 | 1").unwrap().eval(), 3);
-        assert_eq!(parser.parse("3 ^ 1").unwrap().eval(), 2);
-        assert_eq!(parser.parse("1 | 6 ^ 7 & 12").unwrap().eval(), 3);
-        assert_eq!(parser.parse("(1 | 6) ^ 7 & 12").unwrap().eval(), 3);
-        assert_eq!(parser.parse("1 | (6 ^ 7) & 12").unwrap().eval(), 1);
-
-        assert_eq!(parser.parse("3 << 2").unwrap().eval(), 12);
-        assert_eq!(parser.parse("12 >> 2").unwrap().eval(), 3);
-    }
-
-    #[test]
-    fn malformed() {
-        let parser = ExprParser::new();
-
-        assert_matches!(
-            parser.parse("1000000000000").unwrap_err(),
-            ParseError::User{error: ast::Error::LitParse(_)},
-        );
-        assert_matches!(
-            parser.parse("0xg").unwrap_err(),
-            ParseError::InvalidToken{..},
-        );
-        assert_matches!(
-            parser.parse("0x1000000000000").unwrap_err(),
-            ParseError::User{error: ast::Error::LitParse(_)},
-        );
-        assert_matches!(
-            parser.parse("0o9").unwrap_err(),
-            ParseError::InvalidToken{..},
-        );
-        assert_matches!(
-            parser.parse("0o1000000000000").unwrap_err(),
-            ParseError::User{error: ast::Error::LitParse(_)},
-        );
-        parser.parse("10 + 1)").unwrap_err();
-        parser.parse("10 ++ 1)").unwrap_err();
-        parser.parse("10 += 1)").unwrap_err();
-        parser.parse("(10 + 1").unwrap_err();
-        parser.parse("10)( + 1").unwrap_err();
-        parser.parse("10() + 1").unwrap_err();
-    }
-
-    #[test]
-    fn unary() {
-        let parser = ExprParser::new();
-
-        assert_eq!(parser.parse("-1").unwrap().eval(), -1i32 as u32);
-        assert_eq!(parser.parse("-0").unwrap().eval(), -0i32 as u32);
-        assert_eq!(parser.parse("-5 - 6").unwrap().eval(), (-5i32 as u32).wrapping_sub(6));
-        assert_eq!(parser.parse("-5 + 6").unwrap().eval(), (-5i32 as u32).wrapping_add(6));
-        assert_eq!(parser.parse("-5 + -6").unwrap().eval(), (-5i32 as u32).wrapping_add(-6i32 as u32));
-
-        assert_eq!(parser.parse("!0").unwrap().eval(), !0);
-        assert_eq!(parser.parse("!1").unwrap().eval(), !1);
-        assert_eq!(parser.parse("!32").unwrap().eval(), !32);
-        assert_eq!(parser.parse("!(-32)").unwrap().eval(), !(-32i32 as u32));
-    }
-
-    #[test]
-    fn radix_literal() {
-        let parser = ExprParser::new();
-
-        assert_eq!(parser.parse("0xf").unwrap().eval(), 15);
-        assert_eq!(parser.parse("0o20").unwrap().eval(), 16);
-        assert_eq!(parser.parse("0xf ^ 0o20").unwrap().eval(), 31);
-    }
-}
-
 
